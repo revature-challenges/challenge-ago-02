8753703aaf8b54058892b7f58ca40d8210741a9c
ozzy castillo <oswaldo.castillo@revature.net>
2021-08-05 19:52:18 -0500

    ProblemTwo final changes

--- src/com/revature/challenge/problems/MainChallengeAgo02ProblemTwo.java
+++ src/com/revature/challenge/problems/MainChallengeAgo02ProblemTwo.java
@@ -1,18 +1,20 @@
-package com.revature.challenge.problems.two;
+package com.revature.challenge.problems;
+
+import com.revature.challenge.problems.two.ProblemTwo;
 
 import java.util.Arrays;
 
 public class MainChallengeAgo02ProblemTwo {
 
     public static void main(String[] args){
-        int max = 10;
+        int max = 100;
         int min=0;
-        int[] array = new int[max-1];
+        int[] array = new int[max];
         for (int i = 0; i < array.length; i++) {
-            array[i]=(int)((Math.random())*(max-min)) +min;
+            array[i]=(int)((Math.random())*max) +min;
         }
 
-        System.out.println(Arrays.toString(array));
+        ProblemTwo.minAndMaxValuesFromArray(array);
     }
 
 }
--- src/com/revature/challenge/problems/two/ProblemTwo.java
+++ src/com/revature/challenge/problems/two/ProblemTwo.java
@@ -1,2 +1,16 @@
-package com.revature.challenge.problems.two;public class ProblemTwo {
+package com.revature.challenge.problems.two;
+
+import java.util.Arrays;
+
+public class ProblemTwo {
+
+    public static void minAndMaxValuesFromArray(int[] array){
+        System.out.println("This is the Array >>> "+Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("The min values is >> "+array[0]);
+        System.out.println("The max values is >> "+array[array.length-1]);
+
+    }
+
+
 }
